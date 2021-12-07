de2c8f17600469158d93983c2066a46dbf0208f9
Michael Sanford <sanford.mj@gmail.com>
2021-12-06 18:54:14 -0600

    Base code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+int pin = 18;
+int lightTime = 1000;
+int dimTime = 200;
+
+using GpioController controller = new();
+controller.OpenPin(pin, PinMode.Output);
+
+Console.WriteLine($"GPIO pin enabled: {pin}");
+
+Console.CancelKeyPress += new ConsoleCancelEventHandler(myHandler);
+
+void myHandler(object sender, ConsoleCancelEventArgs args)
+{
+
+    controller.Write(pin, PinValue.Low);
+    controller.Dispose();
+}
+
+while (true)
+{
+    Console.WriteLine($"Light for {lightTime}ms");
+    controller.Write(pin, PinValue.High);
+    Thread.Sleep(lightTime);
+
+    Console.WriteLine($"Dim for {dimTime}ms");
+    controller.Write(pin, PinValue.Low);
+    Thread.Sleep(dimTime);
+}
